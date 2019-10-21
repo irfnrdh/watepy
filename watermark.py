407f54437f7826825ecf7de421b836be29f460cb
Irfannur Diah <kang.fand@gmail.com>
2019-10-21 16:56:49 +0700

    Update watermark.py

--- watermark.py
+++ watermark.py
@@ -6,7 +6,6 @@ import sys
 
 from PIL import Image
 
-
 def getListOfFiles(dirName):
 
     # create a list of file and sub directories
@@ -45,48 +44,75 @@ def main():
         namee = os.path.basename(elem)
         (filename, file_extension) = os.path.splitext(namee)
         
-        print ('hell -', filename)
+        print ('Detection -', elem)
 
-        # folder
-        gambar = namee
-        (filename, file_extension) = os.path.splitext(gambar)
+        if file_extension == '.jpg' or file_extension == '.png' :
+            # mengambil gambar
+            image = Image.open(elem)
+        
+            imageWidth = image.width
+            imageHeight = image.height
 
-        # mengambil gambar
-        folder = 'photo/'
-        image = Image.open(folder + filename + file_extension)
-        imageWidth = image.width
-        imageHeight = image.height
+            # folder
+            gambar = namee
+            (filename, file_extension) = os.path.splitext(gambar)
 
-        # mengambil logo
-        logo = Image.open('watermark/logo-entmart-20.png')
+            # mengambil logo
+            logo = Image.open('watermark/logo-entmart-20.png')
 
-        logoWidth = logo.width
-        logoHeight = logo.height
+            logoWidth = logo.width
+            logoHeight = logo.height
 
-        # merisize logo 50% berdasarkan ukuran gambar
-        rl_width = int(imageWidth * 50 / 100)
-        rl_height = int(imageHeight * 7 / 100)
+            #mendapatkan potrait, landscape dan squere dan merisize 50%
+            hasil = imageHeight- imageWidth 
+            
+            if hasil == 0:
+                rl_width = int(imageWidth * 50 / 100)
+                rl_height = int(imageHeight * 50 / 100)
+                print("squere")
 
-        # Resize gambar
-        logoh = logo.resize((rl_width, rl_height), Image.ANTIALIAS)
+            #masih perlu optimasi
+            elif hasil >= 0 and hasil <= 50 :
+                rl_width = int(imageWidth * 50 / 100)
+                rl_height = int(imageHeight * 50 / 100)
+                print("Antara Squere dan Potrait")
+            elif hasil >= 50 :   
+                rl_width = int(imageWidth * 50 / 100)
+                rl_height = int(imageHeight * 35 / 100)
+                print("Potrait")
 
-        # Posisi Tengah
-        position = (int((imageWidth - rl_width) / 2), int((imageHeight
-                    - rl_height) / 2))
+            #masih perlu optimasi
+            elif hasil <= 0 and hasil <=(-50):
+                rl_width = int(imageWidth * 30 / 100)
+                rl_height = int(imageHeight * 50 / 100)
+                print("Antara Potrait dan landscape")
+            elif hasil <=(-50):
+                rl_width = int(imageWidth * 30 / 100)
+                rl_height = int(imageHeight * 50 / 100)
+                print("landscape")
 
-        # Top Left  ....-> (0, 0)
-        # Top Right ....-> (imageWidth - logoWidth, 0)
-        # Bottom Left ....-> (0, imageHeight - logoHeight)
-        # Bottom Right  -> (imageWidth - logoWidth, imageHeight - logoHeight)
+            
+            # Resize gambar
+            logoh = logo.resize((rl_width, rl_height), Image.ANTIALIAS)
 
-        # menyimpan gambar
+            # Posisi Tengah
+            position = (int((imageWidth - rl_width) / 2), int((imageHeight
+                        - rl_height) / 2))
 
-        image.paste(logoh, position, logoh)
-        image.save('output/' + gambar)
+            # Top Left  ....-> (0, 0)
+            # Top Right ....-> (imageWidth - logoWidth, 0)
+            # Bottom Left ....-> (0, imageHeight - logoHeight)
+            # Bottom Right  -> (imageWidth - logoWidth, imageHeight - logoHeight)
+
+            # menyimpan gambar
+
+            image.paste(logoh, position, logoh)
+            image.save('output/' + gambar)
 
 
 if __name__ == '__main__':
     main()
+    print("Mantull")
 
 
             
