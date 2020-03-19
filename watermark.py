0154f07cb135cf187e9537644a23f9046a73295a
irfnrdh <kang.fand@gmail.com>
2020-03-19 14:24:18 +0700

    update

--- watermark.py
+++ watermark.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python
 # coder Irfannur Diah - irfnrdh.com
 
+# rilis  1 - 21 Okt 19
+# Update 2 - 19 Mar 20
+
 import os
 import sys
 
 from PIL import Image
 
+# mendapatkan semua file dalam folder
+
+
 def getListOfFiles(dirName):
-
-    # create a list of file and sub directories
-    # names in the given directory
-
     listOfFile = os.listdir(dirName)
     allFiles = list()
 
-    # Iterate over all the entries
-
     for entry in listOfFile:
-
-        # Create full path
-
         fullPath = os.path.join(dirName, entry)
-
-        # If entry is a directory then get the list of files in this directory
-
         if os.path.isdir(fullPath):
             allFiles = allFiles + getListOfFiles(fullPath)
         else:
@@ -31,25 +25,33 @@ def getListOfFiles(dirName):
 
     return allFiles
 
-def main():
-    dirName = 'photo'
+# menambahkan watermark
 
-    # Get the list of all files in directory tree at given path
 
+def watermark(gambar, watermark, posisi, output):
+    gambar = Image.open(gambar)
+    watermark = Image.open(watermark)
+
+    gambar.paste(watermark, posisi, mask=watermark)
+    gambar.show()
+    gambar.save(output)
+
+
+def main(folder, watermark):
+    dirName = folder
     listOfFiles = getListOfFiles(dirName)
-
-    # Print the files
 
     for elem in listOfFiles:
         namee = os.path.basename(elem)
         (filename, file_extension) = os.path.splitext(namee)
-        
-        print ('Detection -', elem)
 
-        if file_extension == '.jpg' or file_extension == '.png' :
+        if file_extension == '.jpg' or file_extension == '.png':
+
+            print('Detection - ', elem)
+
             # mengambil gambar
             image = Image.open(elem)
-        
+
             imageWidth = image.width
             imageHeight = image.height
 
@@ -58,46 +60,34 @@ def main():
             (filename, file_extension) = os.path.splitext(gambar)
 
             # mengambil logo
-            logo = Image.open('watermark/logo-entmart-20.png')
+            logo = Image.open(watermark)
 
             logoWidth = logo.width
             logoHeight = logo.height
 
-            #mendapatkan potrait, landscape dan squere dan merisize 50%
-            hasil = imageHeight- imageWidth 
-            
+            # mendapatkan potrait, landscape dan squere dan merisize 50%
+            hasil = imageHeight - imageWidth
+
+            if hasil > 0:
+                # tergantung ukuran watermark
+                rl_width = int(imageWidth * 50 / 100)
+                rl_height = int(imageHeight * 6 / 100)
+                print("Detection -  Potrait")
+            elif hasil < 0:
+                rl_width = int(imageWidth * 35 / 100)
+                rl_height = int(imageHeight * 8 / 100)
+                print("Detection -  Landscape")
             if hasil == 0:
                 rl_width = int(imageWidth * 50 / 100)
                 rl_height = int(imageHeight * 50 / 100)
-                print("squere")
+                print("Detection -  Squere")
 
-            #masih perlu optimasi
-            elif hasil >= 0 and hasil <= 50 :
-                rl_width = int(imageWidth * 50 / 100)
-                rl_height = int(imageHeight * 50 / 100)
-                print("Antara Squere dan Potrait")
-            elif hasil >= 50 :   
-                rl_width = int(imageWidth * 50 / 100)
-                rl_height = int(imageHeight * 35 / 100)
-                print("Potrait")
-
-            #masih perlu optimasi
-            elif hasil <= 0 and hasil <=(-50):
-                rl_width = int(imageWidth * 30 / 100)
-                rl_height = int(imageHeight * 50 / 100)
-                print("Antara Potrait dan landscape")
-            elif hasil <=(-50):
-                rl_width = int(imageWidth * 30 / 100)
-                rl_height = int(imageHeight * 50 / 100)
-                print("landscape")
-
-            
             # Resize gambar
             logoh = logo.resize((rl_width, rl_height), Image.ANTIALIAS)
 
             # Posisi Tengah
             position = (int((imageWidth - rl_width) / 2), int((imageHeight
-                        - rl_height) / 2))
+                                                               - rl_height) / 2))
 
             # Top Left  ....-> (0, 0)
             # Top Right ....-> (imageWidth - logoWidth, 0)
@@ -105,14 +95,11 @@ def main():
             # Bottom Right  -> (imageWidth - logoWidth, imageHeight - logoHeight)
 
             # menyimpan gambar
-
             image.paste(logoh, position, logoh)
             image.save('output/' + gambar)
 
 
 if __name__ == '__main__':
-    main()
-    print("Mantull")
-
-
-            
+    folder = 'photo'
+    watermark = 'watermark/logo-entmart-20.png'
+    main(folder, watermark)
